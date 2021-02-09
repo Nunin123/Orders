6ed19e68b9cf787609b894c7b231e0bfcfa7954d
lor <lorraine.manalo.iics@ust.edu.ph>
2021-02-09 16:22:17 +0800

    first commit

---
+++ Forms/Main.vb
@@ -0,0 +1,15 @@
+﻿Public Class frmMain
+    Private Sub btnManageOrder_Click(sender As Object, e As EventArgs) Handles btnManageOrder.Click
+        Dim ManageOrder As New frmMainScreen
+
+        ManageOrder.Show()
+    End Sub
+
+    Private Sub btnManageStock_Click(sender As Object, e As EventArgs) Handles btnManageStock.Click
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
